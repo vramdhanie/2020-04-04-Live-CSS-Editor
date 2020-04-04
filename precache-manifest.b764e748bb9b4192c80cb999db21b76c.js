f4e80d66b4d5c72e0a06964fc98735f697fd8bba
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-04-04 14:57:57 -0400

    Updates

---
+++ precache-manifest.b764e748bb9b4192c80cb999db21b76c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ce75175cbb1b8c7b778b1c18ff92d5",
+    "url": "/2020-04-04-Live-CSS-Editor/index.html"
+  },
+  {
+    "revision": "fe808d47e97925cae6e5",
+    "url": "/2020-04-04-Live-CSS-Editor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d1911873680579da1708",
+    "url": "/2020-04-04-Live-CSS-Editor/static/js/2.82859b44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020-04-04-Live-CSS-Editor/static/js/2.82859b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe808d47e97925cae6e5",
+    "url": "/2020-04-04-Live-CSS-Editor/static/js/main.c3792faf.chunk.js"
+  },
+  {
+    "revision": "f88a2e16fc82e7452849",
+    "url": "/2020-04-04-Live-CSS-Editor/static/js/runtime-main.6e43d831.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2020-04-04-Live-CSS-Editor/static/media/logo.5d5d9eef.svg"
+  }
+]);
